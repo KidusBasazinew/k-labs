aee5028fccfc9412709c9e0e829eea01b1a0588f
Kidus Basazinew <kidusbw@gmail.com>
2024-10-13 06:36:40 -0700

    update on images location 2

--- src/components/lists/ProjectList.ts
+++ src/components/lists/ProjectList.ts
@@ -6,42 +6,42 @@ export interface Product {
 
 export const ProductList: Product[] = [
   {
-    image: "../../public/TravelAgencyLandingPage.jpg",
+    image: "/TravelAgencyLandingPage.jpg",
     websiteUrl: "fakewebsite1.com",
     price: 120,
   },
   {
-    image: "../../public/DigitalMarketingAgencylandingpagedesign.jpg",
+    image: "/DigitalMarketingAgencylandingpagedesign.jpg",
     websiteUrl: "fakewebsite1.com",
     price: 1000,
   },
   {
-    image: "../../MarketingAgencyWebsite.jpg",
+    image: "/MarketingAgencyWebsite.jpg",
     websiteUrl: "fakewebsite2.com",
     price: 2000,
   },
   {
-    image: "../../public/AditalDigitalAgencyWebsite.jpg",
+    image: "/AditalDigitalAgencyWebsite.jpg",
     websiteUrl: "fakewebsite3.com",
     price: 1500,
   },
   {
-    image: "../../public/TravelAgencyLandingPage.jpg",
+    image: "/TravelAgencyLandingPage.jpg",
     websiteUrl: "fakewebsite1.com",
     price: 120,
   },
   {
-    image: "../../public/DigitalMarketingAgencylandingpagedesign.jpg",
+    image: "/DigitalMarketingAgencylandingpagedesign.jpg",
     websiteUrl: "fakewebsite1.com",
     price: 1000,
   },
   {
-    image: "../../MarketingAgencyWebsite.jpg",
+    image: "/MarketingAgencyWebsite.jpg",
     websiteUrl: "fakewebsite2.com",
     price: 2000,
   },
   {
-    image: "../../public/AditalDigitalAgencyWebsite.jpg",
+    image: "/AditalDigitalAgencyWebsite.jpg",
     websiteUrl: "fakewebsite3.com",
     price: 1500,
   },
--- src/components/lists/ServicesLists.ts
+++ src/components/lists/ServicesLists.ts
@@ -7,31 +7,31 @@ export interface Services {
 export const ServiceLists: Services[] = [
   {
     name: "Website Develeopment",
-    icon: "../../public/services/web-development.png",
+    icon: "/services/web-development.png",
     paragraph:
       "Crafting dynamic, user-friendly websites tailored to your business needs.",
   },
   {
     name: "Start ups",
-    icon: "../../public/services/start-up-company.png",
+    icon: "/services/start-up-company.png",
     paragraph:
       "Empowering startups with essential tools and resources for successful growth.",
   },
   {
     name: "Online marketing",
-    icon: "../../public/services/online-marketing.png",
+    icon: "/services/online-marketing.png",
     paragraph:
       "Boosting your brand’s visibility through strategic digital marketing solutions.",
   },
   {
     name: "Motion graphics",
-    icon: "../../public/services/motion-graphic.png",
+    icon: "/services/motion-graphic.png",
     paragraph:
       "Creating captivating motion graphics that enhance storytelling and engagement.",
   },
   {
     name: "Logo design",
-    icon: "../../public/services/logo-design.png",
+    icon: "/services/logo-design.png",
     paragraph:
       "Designing memorable logos that capture your brand's essence and identity.",
   },
